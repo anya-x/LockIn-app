2320ae4ba6134fd8d79a56ce3ac3354e3945add9
lavanya 🐸 <ras.lavanya@gmail.com>
2025-04-04 12:34:42 +0200

    🎉init :(bis) springboot structure wasn't committed somehow

---
+++ backend/src/main/java/com/lockin/lockin_app/LockinAppApplication.java
@@ -0,0 +1,13 @@
+package com.lockin.lockin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockinAppApplication.class, args);
+	}
+
+}
